68ad673d4c128844adc777a4cfe8c18479cfe605
Avangardio <12lol34lol56lol@gmail.com>
2022-11-13 20:26:56 +0300

    Change "Home" route content.

--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -2,17 +2,16 @@ import React, {useEffect, useState} from 'react';
 
 export default function Home() {
 
-    const [data, dataUpdate] = useState({data: ''});
     const today = new Date().toLocaleString('ru', {
         year: 'numeric',
         month: 'long',
         day: 'numeric'
     });
-//pl228
+
     return (
         <div>
-            Добро пожаловать на сайт проекта Friends Club!
-            Сегодня {today}
+            Добро пожаловать на сайт проекта <b>Friends Club!</b>
+            <b>Сегодня {today}</b>
             Код проекта и прочее можно найти <a href={"https://github.com/Avangardio/FCService"} target={'_blank'}>здесь</a>.
             Почта для связи с автором: <b>avangardio1458@gmail.com</b>.
             Благодарю, хорошего дня!
